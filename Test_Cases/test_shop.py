a82ca5603f5bcd0de4cf4df4f61cbd6b5fdf1414
kuangjunhao <m15526494113@163.com>
2021-07-28 18:43:03 +0800

    更新-测试编辑店铺

--- Test_Cases/test_shop.py
+++ Test_Cases/test_shop.py
@@ -8,73 +8,73 @@ from scripts.handle_context import Context, ShopId
 from libs.ddt import ddt, data
 
 
-# @ddt
-# class TestAddShop(unittest.TestCase):
-#     """
-#     测试新增店铺类
-#     """
-#     do_excel = HandleExcel(cases_path, "shop")
-#     cases = do_excel.get_cases()
-#     @classmethod
-#     def setUpClass(cls):
-#         """
-#         建立请求
-#         :return:
-#         """
-#         cls.res = HandleRequest()
-#         cls.token = cls.res.get_token()
-#         cls.mysql = HandleMysql(host=do_config.get_value("mysql", "host"),
-#                                 user=do_config.get_value("mysql", "user"),
-#                                 pwd=do_config.get_value("mysql", "password"),
-#                                 db=do_config.get_value("mysql", "service_db"),
-#                                 port=do_config.get_int("mysql", "port"))
-#
-#     @classmethod
-#     def tearDownClass(cls):
-#         """
-#         关闭请求
-#         :return:
-#         """
-#         cls.res.close_session()
-#         cls.mysql.close_sql()
-#
-#     @data(*cases)
-#     def test_insert_shop(self, one_case):
-#         """
-#         测试新增店铺接口
-#         :param one_case:请求数据
-#         :return:
-#         """
-#         # replace_data = one_case["data"]
-#         # print(replace_data)
-#         new_data = Context.shop_name_param(one_case["data"]) #参数化后的请求数据
-#         print(new_data)
-#         url = do_config.get_value("urlhost", "url") + one_case["url"]
-#         head = {"access-token": self.token}
-#         sql = one_case["check_sql"]
-#         if sql is not None:
-#             sql_data = self.mysql.run_sql(sql)
-#             id_before =sql_data["count"]
-#         result = self.res.do_request(
-#                                     url=url,
-#                                     data=new_data,
-#                                     is_json=True,
-#                                     head=head)
-#         expect_result = one_case["expected"]
-#         msg = one_case["title"]
-#         success_msg = do_config.get_value("msg", "success_result")
-#         fail_msg = do_config.get_value("msg", "fail_result")
-#         try:
-#             self.assertIn(expect_result, result.text, msg=msg)
-#             if sql:
-#                 mysql_data = self.mysql.run_sql(sql)
-#                 id = id_before + 1
-#                 id_after =mysql_data["count"]
-#                 self.assertEqual(id, id_after, msg=msg)
-#             TestAddShop.do_excel.write_excel(one_case["case_id"]+1, result.text, success_msg)
-#         except AssertionError as e:
-#             TestAddShop.do_excel.write_excel(one_case["case_id"] + 1, result.text, fail_msg)
-#             raise e
+@ddt
+class TestAddShop(unittest.TestCase):
+    """
+    测试新增店铺类
+    """
+    do_excel = HandleExcel(cases_path, "shop")
+    cases = do_excel.get_cases()
+    @classmethod
+    def setUpClass(cls):
+        """
+        建立请求
+        :return:
+        """
+        cls.res = HandleRequest()
+        cls.token = cls.res.get_token()
+        cls.mysql = HandleMysql(host=do_config.get_value("mysql", "host"),
+                                user=do_config.get_value("mysql", "user"),
+                                pwd=do_config.get_value("mysql", "password"),
+                                db=do_config.get_value("mysql", "service_db"),
+                                port=do_config.get_int("mysql", "port"))
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        关闭请求
+        :return:
+        """
+        cls.res.close_session()
+        cls.mysql.close_sql()
+
+    @data(*cases)
+    def test_insert_shop(self, one_case):
+        """
+        测试新增店铺接口
+        :param one_case:请求数据
+        :return:
+        """
+        # replace_data = one_case["data"]
+        # print(replace_data)
+        new_data = Context.shop_name_param(one_case["data"]) #参数化后的请求数据
+        print(new_data)
+        url = do_config.get_value("urlhost", "url") + one_case["url"]
+        head = {"access-token": self.token}
+        sql = one_case["check_sql"]
+        if sql is not None:
+            sql_data = self.mysql.run_sql(sql)
+            id_before =sql_data["count"]
+        result = self.res.do_request(
+                                    url=url,
+                                    data=new_data,
+                                    is_json=True,
+                                    head=head)
+        expect_result = one_case["expected"]
+        msg = one_case["title"]
+        success_msg = do_config.get_value("msg", "success_result")
+        fail_msg = do_config.get_value("msg", "fail_result")
+        try:
+            self.assertIn(expect_result, result.text, msg=msg)
+            if sql:
+                mysql_data = self.mysql.run_sql(sql)
+                id = id_before + 1
+                id_after =mysql_data["count"]
+                self.assertEqual(id, id_after, msg=msg)
+            TestAddShop.do_excel.write_excel(one_case["case_id"]+1, result.text, success_msg)
+        except AssertionError as e:
+            TestAddShop.do_excel.write_excel(one_case["case_id"] + 1, result.text, fail_msg)
+            raise e
 
 
 @ddt
